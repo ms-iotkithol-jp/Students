d28d437c8dc6dc90049f6795614bf027da739a00
ms-iotkithol-jp <ms-iotkithol-jp@outlook.jp>
2016-06-21 10:20:46 +0900

    Add supporting code

---
+++ Support/Codes/dbo.Table.MLSensor.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MLSensor] (
+    [msgId]      NCHAR (25) NOT NULL,
+    [deviceId]   TEXT       NOT NULL,
+    [time]       DATETIME   NOT NULL,
+    [accelx]     REAL       NOT NULL,
+    [accely]     REAL       NOT NULL,
+    [accelz]     REAL       NOT NULL,
+    [TempStatus] NCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([msgId] ASC)
+);
---
+++ Support/Codes/dbo.Table.SASSensor.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[SASSensor] (
+	[PartitionId] NCHAR(64) NOT NULL,
+    [msgId]      NCHAR (25) NOT NULL,
+    [deviceId]   TEXT       NOT NULL,
+    [temp]       REAL       NOT NULL,
+    [brightness] REAL       NULL,
+    [accelx]     REAL       NOT NULL,
+    [accely]     REAL       NOT NULL,
+    [accelz]     REAL       NOT NULL,
+    [time]       DATETIME   NOT NULL,
+    [Longitude]  REAL       NULL,
+    [Latitude]   REAL       NULL,
+	[IoTHub]	TEXT	NULL,
+	[EventEnqueuedUtcTime] DATETIME	NOT NULL,
+	[EventProcessedUtcTime] DATETIME	NOT NULL
+    PRIMARY KEY CLUSTERED ([msgId] ASC)
+);
